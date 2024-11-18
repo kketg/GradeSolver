42e24673a7d1cd055b4778ae5393b0d32bb4594d
kketg <kketg03@gmail.com>
2024-11-18 04:23:56 -0500

    Added comments + readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# Possible Grades Solver
+
+Calculate what grades you need on your remaining assignments to get certain final grades
+
+## Usage
+`python main.py <grades>`
+
+The grades file contains json data with the grade and threshold data
+
+### Minimal example grades file
+```json
+{
+    "thresholds":{
+        "A":90.0
+    },
+    "grades":{
+        "Homework": {
+            "grade": 80.0,
+            "weight": 60.0
+        },
+        "Exam":{
+            "grade": -1,
+            "weight": 40.0
+        }
+    }
+}
+```
+
+This will evaluate the minimum grade needed for the Exam to get a final grade of A (90%)
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ n = len(sys.argv)
 
 if n != 2:
     print("Usage: python main.py <grade-file>")
+    exit(1)
 
 filename = sys.argv[1]
-
 with open(filename) as f:
     dict = json.loads(f.read())
     grades_d = dict["grades"]
@@ -20,17 +20,22 @@ with open(filename) as f:
         print("Weights must add up to 100")
         exit(1)
 
+# Prints out parsed grade data
 print("Grades")
 for g in grades:
     print(f"{g[0]}:\t{(g[1] if 0 <= g[1] <= 100 else "____"):04}\t{g[2]}")
-print("")
-
-currentgrade = sum([(g[1]/100.0) * g[2] for g in grades if 0 <= g[1] <= 100.0])
-print(f"Current Total: {currentgrade}")
 print()
 
-missinggrades = [(g[0], g[2]) for g in grades if g[1] < 0]
+# Finds and prints out the current total of grades * weights
+currentgrade = sum([(g[1]/100.0) * g[2] for g in grades if 0 <= g[1] <= 100.0])
+print(f"Current Total: {currentgrade}:.3f")
+print()
 
+# Finds and prints out the grades (name, weight) to be evaluated
+missinggrades = [(g[0], g[2]) for g in grades if g[1] < 0]
+if len(missinggrades) <= 0:
+    print("No missing grades")
+    exit(1)
 print("Missing Grades:")
 for mg in missinggrades:
     print(f"{mg[0]}\t:    {mg[1]}")
@@ -39,13 +44,20 @@ print()
 # x - Grades and their weights (name, weight)
 # result - the final grade desired
 def solve(x, result):
+    # Guess to minimize values
     minimize = [1] * len(x)
     
+    # The equation, i.e. to evaluate two grades (x, y) with weights 20 and 10, currentgrade + 20x + 10y = result
     def constraint(vars):
         return sum([x[i][1] * vars[i] for i in range(len(vars))]) + currentgrade - result
+    
     def objective(vars):
         return sum(vars)
+    
+    # Minimize variables
     minimal = sci.minimize(objective, minimize, bounds = [(0.0,1.0) for _ in range(len(x))], constraints=[{'type': 'eq', 'fun': constraint}], method='SLSQP')
+
+    # Generate some random initial guesses to get randomized grade results that are still valid
     guesses = [[random.uniform(0.05,0.95) for _ in range(len(x))] for _ in range(3)]
     others = [
         sci.minimize(objective, guess, bounds = [(0.0,1.0) for _ in range(len(x))], constraints=[{'type': 'eq', 'fun': constraint}], method='SLSQP')
@@ -58,10 +70,11 @@ def solve(x, result):
         print(f"Error while solving equation: {minimal.message}")
         return [0 for _ in x]
 
-
+# Evaluates and prints grades for each possible threshold
 for name in thresholds:
     val = float(thresholds[name])
     remaining_weights = sum([g[1] for g in missinggrades])
+
     if val > currentgrade + remaining_weights:
         print(f"\nGetting a(n) {name} ({val}) is impossible")
         continue
@@ -69,6 +82,7 @@ for name in thresholds:
         continue
 
     (res, others) = solve(missinggrades, val)
+
     print(f"To get a(n) {name} ({val}):")
     print(f"\tYou need at minimum: ")
     for i in range(len(missinggrades)):
